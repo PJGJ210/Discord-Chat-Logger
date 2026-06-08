e19489961abc03bac710e314b728fb388889bb80
Robbe De Neve <rdeneve00@gmail.com>
2026-06-08 22:44:35 +0200

    Sanitize CA_ID

--- src/main/java/com/discordchatlogger/utils/ChatMessageUtils.java
+++ src/main/java/com/discordchatlogger/utils/ChatMessageUtils.java
@@ -5,7 +5,10 @@ import net.runelite.client.util.Text;
 
 public class ChatMessageUtils {
     public static String getSanitizedAuthor(ChatMessage chatMessage) {
-        return Text.removeTags(chatMessage.getName()).replaceAll("[^0-9a-zA-Z ]+", " ");
+        return Text
+            .removeTags(chatMessage.getName())
+            .replaceAll("CA_ID:\\d+\\|", "")
+            .replaceAll("[^0-9a-zA-Z ]+", " ");
     }
 
     public static String getSanitizedSender(ChatMessage chatMessage) {
