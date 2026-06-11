891c71bda5a0b2bc88c6d6e67e218ecb31f39d43
Robbe De Neve <rdeneve00@gmail.com>
2026-06-11 16:19:32 +0200

    Sanitize CA_ID at the correct location

--- src/main/java/com/discordchatlogger/utils/ChatMessageUtils.java
+++ src/main/java/com/discordchatlogger/utils/ChatMessageUtils.java
@@ -7,20 +7,19 @@ public class ChatMessageUtils {
     public static String getSanitizedAuthor(ChatMessage chatMessage) {
         return Text
             .removeTags(chatMessage.getName())
-            .replaceAll("CA_ID:\\d+\\|", "")
             .replaceAll("[^0-9a-zA-Z ]+", " ");
     }
 
     public static String getSanitizedSender(ChatMessage chatMessage) {
         return Text
             .removeTags(chatMessage.getSender())
-            .replaceAll("CA_ID:\\d+\\|", "")
             .replaceAll("[^0-9a-zA-Z ]+", " ");
     }
 
     public static String getSanitizedChatContent(ChatMessage chatMessage) {
         String messageContent = Text.removeTags(chatMessage.getMessage());
         return messageContent
+                .replaceAll("CA_ID:\\d+\\|", "")
                 .replaceAll("@everyone", "@ everyone")
                 .replaceAll("@here", "@ here")
                 .replaceAll("~", "\\~")
